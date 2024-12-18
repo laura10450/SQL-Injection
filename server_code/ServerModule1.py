89a097adb34df81c820984131728191d663322e2
laura.ruetzler@student.htl-rankweil.at <laura.ruetzler@student.htl-rankweil.at>
2024-12-18 10:38:02 +0000

    Edited settings, ServerModule1 and Form1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,3 +1,8 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.files
+from anvil.files import data_files
 import sqlite3
 import anvil.server
 
